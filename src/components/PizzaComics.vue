9e05aa4c8c95f575a2152938f15d1d7a86557150
coder-ms <marco.sbrascia@gmail.com>
2022-11-22 23:41:50 +0200

    piaazza comics pt.1

--- src/components/PizzaComics.vue
+++ src/components/PizzaComics.vue
@@ -1,9 +1,22 @@
 <template>
     <div class="containerx col-12 col-lg-12 col-md-12 col-sm-12">
-        <div class="imageDescription col-12 col-lg-12 col-md-12 col-sm-12">
+        <div class="imageDescription col-6 col-lg-6 col-md-6 col-sm-6">
             <div class="col-6 col-lg-6 col-md-6 col-sm-6">
-                <div class="descriptionx col-6 col-lg-6 col-md-6 col-sm-6">
-                    <div></div>
+                <div>
+                    <h6>MADE WITH LOVE</h6>
+                    <h2>DELISH PIZZA DEALS</h2>
+                </div>
+
+                <div v-for="(item, index) in MenuArray" :key="item"
+                    class="descriptionx col-8 col-lg-8 col-md-8 col-sm-8">
+                    <div class="col-2 col-lg-2 col-md-2 col-sm-2">
+                        <h3>{{ item.day }}</h3>
+                        <h4>{{ item.month }}</h4>
+                    </div>
+                    <div class="col-10 col-lg-10 col-md-10 col-sm-10">
+                        <h3>{{ item.description }}</h3>
+                        <p><i class="fa-solid fa-location-dot"></i>{{ item.month }}</p>
+                    </div>
                     <!--
                     <h2>SPECIALS*</h2>
                     <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesent ut.</h5>
@@ -14,9 +27,9 @@
                     -->
                 </div>
             </div>
-            <div class="imageComic col-6 col-lg-6 col-md-6 col-sm-6">
-                <img src="../../public/img/h3-img-5a.jpg" alt="Baffo">
-            </div>
+        </div>
+        <div class="imageComic col-6 col-lg-6 col-md-6 col-sm-6">
+            <img src="../../public/img/h3-img-5a.jpg" alt="Baffo">
         </div>
     </div>
 
@@ -33,16 +46,22 @@ export default {
         return {
             MenuArray: [
                 {
-                    dollar: '$10',
-                    combo: 'combo piccolo',
+                    day: '02',
+                    month: 'NOV',
+                    description: 'TRADITIONAL NEAPOLITAN PIES IN KYOTO PIZZA MERCATO',
+                    address: 'combo piccolo',
                 },
                 {
-                    dollar: '$20',
-                    combo: 'combo medio',
+                    day: '03',
+                    month: 'NOV',
+                    description: 'TERRAZZA PATIO DINING SPACE OPENING THIS WEEKEND',
+                    address: '',
                 },
                 {
-                    dollar: '$30',
-                    combo: 'combo grande',
+                    day: '05',
+                    month: 'NOV',
+                    description: 'SIENNA PRIVATE DINING ROOM WITH STEPHANE BRUNN',
+                    address: 'combo piccolo',
                 },
             ],
         }
@@ -55,6 +74,7 @@ export default {
 <style lang="scss" scoped>
 .containerx {
     display: flex;
+    // overflow: hidden;
 
 
     .imageDescription {
@@ -62,52 +82,46 @@ export default {
         justify-content: center;
         align-items: center;
         background-color: black;
+        border: 1px solid white;
 
         div {
+
+
+            h3 {
+                color: white;
+                font-size: 30px;
+                font-weight: 700;
+            }
+
+            h4 {
+                color: rgb(209, 1, 1);
+                font-size: 12px;
+            }
+
             .descriptionx {
-                background-color: white;
-                border: 1px solid grey;
-                width: 60%;
-                margin: 0 auto;
-
-
-                h2 {
-                    font-size: 30px;
-                    font-weight: 700;
-                }
-
-                h5 {
-                    font-size: 18px;
-                }
+                border-bottom: 1px dashed gold;
+                width: 100%;
+                display: flex;
 
                 h3 {
-                    color: rgb(209, 1, 1);
-                    font-size: 25px;
+                    color: white;
+                    font-size: 16px;
                     font-weight: 600;
-                    margin: 20px 0;
-
-                    span {
-                        color: black;
-                        font-size: 25px;
-                        font-weight: 600;
-                        text-transform: uppercase;
-                    }
                 }
 
                 p {
-                    color: grey;
-                    padding-left: 53px;
-                    font-size: 16px;
+                    color: rgb(209, 1, 1);
+                    font-size: 12px;
                 }
             }
         }
 
-        .imageComic {
-            img {
-                width: 100%;
-            }
-        }
+    }
 
+    .imageComic {
+        img {
+            width: 100%;
+        }
     }
 }
 </style>
